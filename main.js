de0b7003c4e3e3de897dcd2a21a5c6a19150c1e0
aaksant <aaksangit@gmail.com>
2023-11-20 19:42:45 +0700

    Fix same pick results in a win bug

--- main.js
+++ main.js
@@ -4,62 +4,76 @@ function getComputerSelection() {
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
-function playGame(totalAttempts) {  
-  let playerWinCount = 0;
-  let computerWinCount = 0;
+function playGame(totalAttempts) {
+  let playerScore = 0;
+  let computerScore = 0;
   let attemptsLeft = 0;
 
+  console.log(`You have ${totalAttempts} attempts.`)
+
   while (attemptsLeft < totalAttempts) {
-    const playerSelection = prompt('Enter your choice').toLowerCase();
+    const playerSelection = prompt("Enter your choice").toLowerCase();
     const computerSelection = getComputerSelection();
 
     if (playerSelection === computerSelection) {
-      console.log("Draw");
-      attemptsLeft++;
+      console.log("Draw")
+    } 
+    
+    else if (playerSelection === 'rock') {
+      if (computerSelection !== 'paper') {
+        console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++;
+      } else {
+        console.log(`Computer wins: ${computerSelection} beats ${playerSelection}`);
+        computerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++
+      }
+    } 
+
+    else if (playerSelection === 'paper') {
+      if (computerSelection !== 'scissors') {
+        console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++;
+      } else {
+        console.log(`Computer wins: ${computerSelection} beats ${playerSelection}`);
+        computerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++
+      }
     }
 
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-      console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
-      playerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
-    } else if (playerSelection === "rock" && computerSelection !== "scissors") {
-      console.log(
-        `Computer wins: ${computerSelection} beats ${playerSelection}`
-      );
-      computerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
+    else if (playerSelection === 'scissors') {
+      if (computerSelection !== 'rock') {
+        console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++;
+      } else {
+        console.log(`Computer wins: ${computerSelection} beats ${playerSelection}`);
+        computerScore++;
+        console.log(`${playerScore} : ${computerScore}`);
+        attemptsLeft++
+      }
     }
 
-    if (playerSelection === "paper" && computerSelection === "rock") {
-      console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
-      playerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
-    } else if (playerSelection === "paper" && computerSelection !== "rock") {
-      console.log(
-        `Computer wins: ${computerSelection} beats ${playerSelection}`
-      );
-      computerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
+    else {
+      console.log('Invalid choice.')
+      continue;
     }
+  }
 
-    if (playerSelection === "scissors" && computerSelection === "paper") {
-      console.log(`Player wins: ${playerSelection} beats ${computerSelection}`);
-      playerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
-    } else if (playerSelection === "scissors" && computerSelection !== "paper") {
-      console.log(
-        `Computer wins: ${computerSelection} beats ${playerSelection}`
-      );
-      computerWinCount++;
-      console.log(`${playerWinCount} : ${computerWinCount}`);
-      attemptsLeft++;
-    }
+  if (playerScore == computerScore) {
+    console.log("Draw, what a boring game.");
+  } else if (playerScore > computerScore) {
+    console.log("You win!");
+  } else {
+    console.log("You lose!");
   }
 }
 
-playGame(5)
+playGame(5);
