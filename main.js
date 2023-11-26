35de69a19b7a8969cd2cf13527704fb1da203677
aaksant <aaksangit@gmail.com>
2023-11-26 17:47:19 +0700

    Early implementation of reset feature

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const choice = ['rock', 'paper', 'scissors'];
 
 function getComputerSelection() {
   return choice[Math.floor(Math.random() * choice.length)];
-};
+}
 
 let playerScore = document.querySelector('.player-score');
 let compScore = document.querySelector('.comp-score');
@@ -17,66 +17,119 @@ const scissorsText = scissorsButton.textContent.toLowerCase();
 
 const condition = document.querySelector('.condition');
 const selections = document.querySelector('.selections');
+const final = document.querySelector('.final');
 
-rockButton.addEventListener('click', () => {
-  let playerNewScore = Number(playerScore.textContent);
-  let compNewScore = Number(compScore.textContent);
-  const compSelection = getComputerSelection();
+let playerNewScore = Number(playerScore.textContent);
+let compNewScore = Number(compScore.textContent);
 
-  if (rockText === compSelection) {
-    condition.textContent = 'draw';
-    selections.textContent = `both pick ${rockText}`;
-  } else if (compSelection !== 'paper') {
-    playerNewScore++;
-    playerScore.textContent = playerNewScore;
-    condition.textContent = 'you win';
-    selections.textContent = `rock beats ${compSelection}`;
-  } else {
-    compNewScore++;
-    compScore.textContent = compNewScore;
-    condition.textContent = 'you lose';
-    selections.textContent = `${compSelection} beats rock`
+const maxAttempts = 10;
+let attempt = 0;
+
+function updateAttempt() {
+  attempt++;
+  if (attempt === maxAttempts + 1) {
+    rockButton.removeEventListener('click', rockButtonHandler);
+    paperButton.removeEventListener('click', paperButtonHandler);
+    scissorsButton.removeEventListener('click', scissorsButtonHander);
   }
-});
+}
 
-paperButton.addEventListener('click', () => {
-  let playerNewScore = Number(playerScore.textContent);
-  let compNewScore = Number(compScore.textContent);
-  const compSelection = getComputerSelection();
+function reset() {
+  attempt = 0;
+  rockButton.addEventListener('click', rockButtonHandler);
+  paperButton.addEventListener('click', paperButtonHandler);
+  scissorsButton.addEventListener('click', scissorsButtonHander);
 
-  if (paperText === compSelection) {
-    condition.textContent = 'draw';
-    selections.textContent = `both pick ${paperText}`;
-  } else if (compSelection !== 'scissors') {
-    playerNewScore++;
-    playerScore.textContent = playerNewScore;
-    condition.textContent = 'you win';
-    selections.textContent = `paper beats ${compSelection}`;
-  } else {
-    compNewScore++;
-    compScore.textContent = compNewScore;
-    condition.textContent = 'you lose';
-    selections.textContent = `${compSelection} beats paper`
+  playerNewScore = 0;
+  compNewScore = 0;
+  playerScore.textContent = '0';
+  compScore.textContent = '0';
+  condition.textContent = '';
+  selections.textContent = '';
+  final.textContent = '';
+}
+
+function checkWinner(playerScore, compScore) {
+  if (attempt === maxAttempts + 1) {
+    if (playerScore > compScore) {
+      condition.textContent = '';
+      selections.textContent = '';
+      final.textContent = 'you win!';
+    } else {
+      condition.textContent = '';
+      selections.textContent = '';
+      final.textContent = 'you lose!';
+    }
+    reset();
   }
-});
+}
 
-scissorsButton.addEventListener('click', () => {
-  let playerNewScore = Number(playerScore.textContent);
-  let compNewScore = Number(compScore.textContent);
-  const compSelection = getComputerSelection();
+function rockButtonHandler() {
+    const compSelection = getComputerSelection();
+  
+    if (rockText === compSelection) {
+      condition.textContent = 'draw';
+      selections.textContent = `both pick ${rockText}`;
+    } else if (compSelection !== 'paper') {
+      playerNewScore++;
+      playerScore.textContent = playerNewScore;
+      condition.textContent = 'you win';
+      selections.textContent = `rock beats ${compSelection}`;
+    } else {
+      compNewScore++;
+      compScore.textContent = compNewScore;
+      condition.textContent = 'you lose';
+      selections.textContent = `${compSelection} beats rock`;
+    }
 
-  if (scissorsText === compSelection) {
-    condition.textContent = 'draw';
-    selections.textContent = `both pick ${scissorsText}`;
-  } else if (compSelection !== 'rock') {
-    playerNewScore++;
-    playerScore.textContent = playerNewScore;
-    condition.textContent = 'you win';
-    selections.textContent = `scissors beats ${compSelection}`;
-  } else {
-    compNewScore++;
-    compScore.textContent = compNewScore;
-    condition.textContent = 'you lose';
-    selections.textContent = `${compSelection} beats scissors`
-  }
-});
+    updateAttempt();
+    checkWinner(playerNewScore, compNewScore);
+}
+
+function paperButtonHandler() {
+    const compSelection = getComputerSelection();
+
+    if (paperText === compSelection) {
+      condition.textContent = "draw";
+      selections.textContent = `both pick ${paperText}`;
+    } else if (compSelection !== "scissors") {
+      playerNewScore++;
+      playerScore.textContent = playerNewScore;
+      condition.textContent = "you win";
+      selections.textContent = `paper beats ${compSelection}`;
+    } else {
+      compNewScore++;
+      compScore.textContent = compNewScore;
+      condition.textContent = "you lose";
+      selections.textContent = `${compSelection} beats paper`;
+    }
+
+    updateAttempt();
+    checkWinner(playerNewScore, compNewScore);
+}
+
+function scissorsButtonHander() {
+    const compSelection = getComputerSelection();
+  
+    if (scissorsText === compSelection) {
+      condition.textContent = 'draw';
+      selections.textContent = `both pick ${scissorsText}`;
+    } else if (compSelection !== 'rock') {
+      playerNewScore++;
+      playerScore.textContent = playerNewScore;
+      condition.textContent = 'you win';
+      selections.textContent = `scissors beats ${compSelection}`;
+    } else {
+      compNewScore++;
+      compScore.textContent = compNewScore;
+      condition.textContent = 'you lose';
+      selections.textContent = `${compSelection} beats scissors`;
+    }
+
+    updateAttempt();
+    checkWinner(playerNewScore, compNewScore);
+}
+
+rockButton.addEventListener('click', rockButtonHandler);
+paperButton.addEventListener('click', paperButtonHandler);
+scissorsButton.addEventListener('click', scissorsButtonHander);
